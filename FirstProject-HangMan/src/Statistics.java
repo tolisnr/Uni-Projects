d0dfc3a78dd643ad33ea8e3f15ae8678621c70ce
Apostolos Sinioris <siniorisap@outlook.com>
2023-11-27 16:14:06 +0200

    Delete FirstProject-HangMan directory

--- FirstProject-HangMan/src/Statistics.java
+++
@@ -1,8 +0,0 @@
-public class Statistics {
-    
-    public static void showStatistics(int games ,int won, int lost) {
-        // TODO implement here
-        System.out.println("You have played so far " + games + " games. You won " 
-                            + won + " times and lost " + lost + " times.\n");
-    }
-}
